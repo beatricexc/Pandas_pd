1dda8d62d9ad0d6d5ae8a544412dd1a4c56ed24d
beatricx <72341578+beatricexc@users.noreply.github.com>
2022-07-10 22:39:12 +0100

    Add files via upload

---
+++ no_corr_scatter_plot.py
@@ -0,0 +1,21 @@
+#Three lines to make our compiler able to draw:
+
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+
+import pandas
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('data.csv')
+
+df.plot(kind = 'scatter', x = 'Duration', y = 'Maxpulse')
+
+df.show()
+
+
+# Two lines to make our compiler able to draw:
+
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
